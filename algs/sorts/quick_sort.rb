42c300dbf38a9138f2087ac4cfc651cc437141c8
Nicholas Esposito <nicholasesposito@Nicholass-MacBook-Air.local>
2013-08-09 13:16:17 -0400

    quick sort complete

--- algs/sorts/quick_sort.rb
+++ algs/sorts/quick_sort.rb
@@ -1,17 +1,18 @@
 a = Array.new
-11.times do
+12.times do
 	a << rand(5000)
 end
 
 def quicksort(array, pass=0)
 
-	array=[array] unless array.class == Array
+	if pass == 0
+		puts "\nOriginal:"
+		print array
+	end
 	
-	if array.length < 2
-		puts "\nEscape"
+	if array.class == Fixnum || array.length < 2
 		return array
 	end
-
 	pos1=0
 	pos2=array.length-1
 	pivot_pos = rand(array.length-1)
@@ -25,34 +26,22 @@ def quicksort(array, pass=0)
 			pos2 -= 1
 		end
 		array[pos1], array[pos2] = array[pos2], array[pos1]
-		puts "\n"
-		print array
 	end
 
 	pivot_pos = array.index(pivot)
-	a_1 = Array.new
-	a_2 = Array.new
-	(0...pivot_pos).each do |i|
-		a_1 << array[i]
-	end
-	(pivot_pos...array.length-1).each do |i|
-		a_2 << array[i]
-	end
-	
-	a_final = Array.new
-	
-		a_1=quicksort(a_1)
-		a_final = a_final + a_1 unless a_1 == nil
-	
-		a_final <<  pivot unless pivot == nil
-		
-		a_2=quicksort(a_2)
-		a_final = a_final + a_2 unless a_2 == nil
-	
-	puts "\nfinal array:"
-	print a_final
-	puts "\n"
 
+	a1=array.slice(0...pivot_pos)
+	a2=array.slice(pivot_pos+1..array.length-1)
+	a_f = Array.new
+	a_f << a1
+	a_f << [pivot]
+	a_f << a2
+	a_f.length.times do |i|
+		a_f[i]=quicksort(a_f[i],pass+1)
+	end
+	puts "\n#{pass} iterations deep"
+	print a_f.flatten!	
+	return a_f
 end
 
 quicksort(a)
